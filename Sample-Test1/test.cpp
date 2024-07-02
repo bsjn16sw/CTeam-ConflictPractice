4aecbef17125433e6d9b8b1ba067c42e6d56d211
cwon.lee <wonmyth@naver.com>
2024-07-02 16:25:55 +0900

    add getSumSum() and gtest code

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,3 +5,10 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(CalClassTestCase, getSumSumTest) {
+	Cal c;
+
+	EXPECT_EQ(c.getSumSum(1, 2, 3), 6);
+	EXPECT_EQ(c.getSumSum(-1, -2, -3), -6);
+}
