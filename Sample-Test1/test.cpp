934b16a398c86911e648d422c9f4821ae4aa8579
Jongin Kim <k.jongin@gmail.com>
2024-07-02 16:34:46 +0900

    TDD처럼 TC부터 commit합니다

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "../CTeam-ConflictPractice/Cal.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, MinusTest) {
+	Cal calc;
+	EXPECT_EQ(calc.getMinus(5, 3), 2);
 }
