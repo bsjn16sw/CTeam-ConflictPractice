addaf70f0d9c603b3383618efd15170de00890fc
sbin.jeon <sbin.jeon@samsung.com>
2024-07-02 16:03:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## CTeam-ConflictPractice
+* getSum(a, b)
+* getGop(a, b)
+* getZegop(a)
+* getMinus(a, b)
+* getDivide(a, b)
+* getSumSum(a, b, c)
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../CTeam-ConflictPractice/Cal.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
